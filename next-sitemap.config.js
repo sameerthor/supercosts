cfea58cf77d0ef8b6aa6b77c67abdc1c5c53480b
shivam <shivam.srtech@gmail.com>
2025-09-24 13:36:04 +0530

    reward pages

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.supercosts.com', // replace with your actual domain
+  generateRobotsTxt: true,            // (optional) generate robots.txt
+  sitemapSize: 5000,                   // (optional) max URLs per sitemap
+};
